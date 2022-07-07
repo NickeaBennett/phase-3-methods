5f7462f193cf7e84d2d7f100e1bddacf933e654e
NickeaBennett <nickeabennett@gmail.com>
2022-07-07 17:26:09 -0400

    Saving grace

--- methods.rb
+++ methods.rb
@@ -1,1 +1,38 @@
 # Your code here!
+
+
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+
+def greet name
+    puts 'Hello + #{name}!'
+end
+
+
+
+
+
+
+
+# function myFunction(param) {
+#     console.log('Running myFunction');
+#     return param + 1;
+# }
+
+# def my_method(param)
+#     puts "Running my_method"
+#     param + 1
+# end
+
+# def say_hello()
+#     'hello'
+# end
+# say_hello()
+# def stylish_painter
+#     best_hairstyle = "Bob Ross"
+#     return "Jean-Michel Basquiat"
+#     best_hairstyle
+#   end
+#   stylish_painter
